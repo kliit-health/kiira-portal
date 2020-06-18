74684296afe6d61338f90e819e29e30d5823385b
Steven Tagloehner <steven.tagloehner@outlook.com>
2020-06-17 23:23:23 -0700

    adds layout component

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,15 @@
 import { UserProvider } from '../firebase'
 import { wrapper } from 'redux/store'
-import { Auth } from '../components'
+import { Auth, Layout } from '../components'
 import '../styles/globals.scss'
 
 const App = ({ Component, pageProps }) => (
 	<UserProvider>
-		<Auth>
-			<Component {...pageProps} />
-		</Auth>
+		<Layout>
+			<Auth>
+				<Component {...pageProps} />
+			</Auth>
+		</Layout>
 	</UserProvider>
 )
 
