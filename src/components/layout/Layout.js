67d0e2e5e7bae3640a5e0d624cd4a5f026fe849f
Steven Tagloehner <steven.tagloehner@outlook.com>
2020-06-17 23:23:23 -0700

    adds layout component

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,17 +1,24 @@
-import { Header } from 'components/header'
+import { cloneElement } from 'react'
+import { IconButton } from '@material-ui/core'
+import ExitToAppIcon from '@material-ui/icons/ExitToApp'
 import { useRouter } from 'next/router'
+import { Sidebar } from 'components'
 import styles from './layout.module.scss'
 
-export const Layout = ({ children, user, ...rest }) => {
+export const Layout = ({ children }) => {
 	const router = useRouter()
 
 	const isLoginPage = router.pathname === '/login'
+
 	return isLoginPage ? (
-		<div>{children}</div>
+		cloneElement(children)
 	) : (
-		<div>
-			<Header {...rest} />
+		<div className={styles.layout}>
+			<Sidebar />
 			{children}
+			{/* <IconButton className={styles['icon-button']} color="primary">
+				<ExitToAppIcon />
+			</IconButton> */}
 		</div>
 	)
 }
