9171a7a869a95ed7a05efda904294bfed95b6352
Steven Tagloehner <steven.tagloehner@outlook.com>
2020-06-19 12:17:29 -0700

    properly hadles login errors

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,14 +1,13 @@
 import React from 'react'
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 import { ServerStyleSheets } from '@material-ui/core/styles'
-import { theme } from '../theme'
 import '../styles/globals.scss'
 
 export default class MyDocument extends Document {
 	render() {
 		return (
 			<Html lang="en">
-				<Head></Head>
+				<Head />
 				<body>
 					<Main />
 					<NextScript />
