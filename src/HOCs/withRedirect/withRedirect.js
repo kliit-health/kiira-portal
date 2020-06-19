3bc431a911eb2490b46f80657401f2c232d1fd26
Steven Tagloehner <steven.tagloehner@outlook.com>
2020-06-18 22:25:17 -0700

    fixes withRedirect logic

--- src/HOCs/withRedirect/withRedirect.js
+++ src/HOCs/withRedirect/withRedirect.js
@@ -1,34 +1,33 @@
 import { useRouter } from 'next/router'
 import { isClient } from 'helpers/functions'
 
-/**
- * @description redirect to specified route
- * @param { string } location path of the location you want to redirect. Defaults to "/login".
- * @param { boolean } requireAuth  whether authentication is required in order to redirect. Defaults to false.
- */
-
 // TODO: Server side implementation is missing
 
+const isBrowser = isClient()
+
 export const withRedirect = (
-	location = '/login',
-	requireAuth = false
+	key,
+	location,
+	isProtected = true
 ) => WrappedComponent => {
 	const WithRedirectWrapper = props => {
-		const { details, loading } = props.user
 		const router = useRouter()
 
-		if (loading) return null
-
-		if (requireAuth && details && isClient()) {
-			router.push(location)
+		if (!props[key] && isProtected && isBrowser) {
+			router.replace(location)
 			return null
 		}
 
-		if (!details && !requireAuth && isClient()) {
-			router.push(location)
+		if (props[key] && !isProtected && isBrowser) {
+			router.replace(location)
 			return null
 		}
+
 		return <WrappedComponent {...props} />
+	}
+	WithRedirectWrapper.getInitialProps = async context => {
+		// TODO: implement server side redirect
+		return {}
 	}
 	return WithRedirectWrapper
 }
