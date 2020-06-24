de23caffb048b7ba2abe223fd7c75c6f28d08dc0
Steven Taglohner <steven.taglohner@gmail.com>
2020-06-24 15:35:32 -0700

    adds firebase fetch hook

---
+++ src/hooks/useFirebaseFetch.js
@@ -0,0 +1,29 @@
+import { useEffect, useState } from 'react'
+import { firestore } from '../firebase'
+
+export const useFirebaseFetch = (collectionName, condition, limit = 1000) => {
+	const [data, setData] = useState(null)
+	const [loading, setLoading] = useState(true)
+	const [error, setError] = useState(null)
+
+	useEffect(() => {
+		const queryFirestore = async () => {
+			try {
+				const collection = firestore.collection(collectionName)
+				const query = collection.where(...condition).limit(limit)
+				const response = await query.get()
+				if (response) {
+					const data = response.docs.map(item => item.data())
+					setData(data)
+				}
+			} catch (error) {
+				setError(error)
+			} finally {
+				setLoading(false)
+			}
+		}
+		queryFirestore()
+	}, [])
+
+	return { loading, data, error }
+}
