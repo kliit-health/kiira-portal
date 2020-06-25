0e27e0230f0f628b39fdaab1c95fe23757175a94
Steven Taglohner <steven.taglohner@gmail.com>
2020-06-25 13:45:32 -0700

    adds dynamic page for the expert profile

--- src/firebase/initializer.js
+++ src/firebase/initializer.js
@@ -3,28 +3,15 @@ import firebase from 'firebase/app'
 import 'firebase/auth'
 import 'firebase/firestore'
 
-const { publicRuntimeConfig } = getConfig()
-
-const {
-	FIREBASE_API_KEY,
-	FIREBASE_AUTH_DOMAIN,
-	FIREBASE_DATABASE_URL,
-	FIREBASE_PROJECT_ID,
-	FIREBASE_STORAGE_BUCKET,
-	FIREBASE_MESSAGING_SENDER_ID,
-	FIREBASE_APP_ID,
-	FIREBASE_MEASUREMENT_ID
-} = publicRuntimeConfig
-
 const config = {
-	apiKey: FIREBASE_API_KEY,
-	authDomain: FIREBASE_AUTH_DOMAIN,
-	databaseURL: FIREBASE_DATABASE_URL,
-	projectId: FIREBASE_PROJECT_ID,
-	storageBucket: FIREBASE_STORAGE_BUCKET,
-	messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
-	appId: FIREBASE_APP_ID,
-	measurementId: FIREBASE_MEASUREMENT_ID
+	apiKey: process.env.FIREBASE_API_KEY,
+	authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+	databaseURL: process.env.FIREBASE_DATABASE_URL,
+	projectId: process.env.FIREBASE_PROJECT_ID,
+	storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+	appId: process.env.FIREBASE_APP_ID,
+	measurementId: process.env.FIREBASE_MEASUREMENT_ID
 }
 
 firebase.apps.length
---
+++ src/pages/dashboard/experts/[uid]/expert.js
@@ -0,0 +1,5 @@
+import { Container } from 'components'
+
+export const Expert = () => {
+	return <Container />
+}
