72456577a3b7552411af9be689345a1e379f3561
Steven Taglohner <steven.taglohner@gmail.com>
2020-06-25 13:16:22 -0700

    add proper Dotenv

--- next.config.js
+++ next.config.js
@@ -1,18 +1,13 @@
 const withSass = require('@zeit/next-sass')
-const path = require('path')
+const Dotenv = require('dotenv-webpack')
 
 // styles are applyed only after a refresh ** dev only **.
 //github.com/vercel/next.js/issues/10752
 
 https: module.exports = withSass({
-	publicRuntimeConfig: {
-		FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
-		FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
-		FIREBASE_DATABASE_URL: process.env.FIREBASE_DATABASE_URL,
-		FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
-		FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
-		FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
-		FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
-		FIREBASE_MEASUREMENT_ID: process.env.FIREBASE_MEASUREMENT_ID
+	webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+		config.plugins.push(new Dotenv({ silent: true }))
+
+		return config
 	}
 })
