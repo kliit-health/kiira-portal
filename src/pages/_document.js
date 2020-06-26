e477a09a915bf93367d93d5f9568162d038b1099
Steven Taglohner <steven.taglohner@gmail.com>
2020-06-25 17:53:42 -0700

    fixes firefox font issues

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -2,23 +2,13 @@ import React from 'react'
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 import { ServerStyleSheets } from '@material-ui/core/styles'
 import { theme } from '../theme'
+import '../styles/globals.scss'
 
 export default class MyDocument extends Document {
 	render() {
 		return (
 			<Html lang="en">
-				<Head>
-					<meta name="theme-color" content={theme.palette.primary.main} />
-					<link
-						rel="stylesheet"
-						href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-					/>
-					<link href="/fonts/Avenir-Book.ttf" as="font" />
-					<link href="/fonts/Poppins-Regular.otf" as="font" />
-					<link href="/fonts/Poppins-Thin.otf" as="font" />
-					<link href="/fonts/Poppins-Light.otf" as="font" />
-					<link href="/fonts/Poppins-Bold.otf" as="font" />
-				</Head>
+				<Head></Head>
 				<body>
 					<Main />
 					<NextScript />
