0be37ce1a8b0ba4441bf66ea66c411a8b46fab9e
Steven Taglohner <steven.taglohner@gmail.com>
2020-06-29 22:33:37 -0700

    fixes next requires pages to be single react component.

--- src/components/pages/login/Login.js
+++ src/components/pages/login/Login.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { compose } from 'recompose'
 import { withLoadingIndicator, withRedirect } from 'HOCs'
 import { Button, TextField, FormHelperText } from '@material-ui/core'
@@ -11,16 +11,6 @@ const Login = ({ authError }) => {
 	const [error, setError] = useState('')
 	const [email, setEmail] = useState('')
 	const [password, setPassword] = useState('')
-	const [errorCode, setErrorCode] = useState('')
-
-	const styles = {
-		fields: 'login-page__auth-fields',
-		helper: 'login-page__helper-text',
-		page: 'login-page',
-		card: 'login-page__card',
-		onboarding: 'login-page__onboarding',
-		auth: 'login-page__auth'
-	}
 
 	const styles = {
 		fields: 'login-page__auth-fields',
@@ -75,42 +65,6 @@ const Login = ({ authError }) => {
 		<div className={styles.helper}>
 			<FormHelperText error>{getLoginErrorMessage(error)}</FormHelperText>
 		</div>
-	)
-
-	const renderTextFields = () => (
-		<div className={styles['auth-fields-container']}>
-			<TextField
-				label={intl.email.description}
-				onChange={handleOnEmailChange}
-			/>
-			<TextField
-				label={intl.password.description}
-				onChange={handleOnPasswordChange}
-			/>
-		</div>
-	)
-
-	const renderLoginButton = () => (
-		<div className={styles['button-container']}>
-			<Button
-				variant="contained"
-				color="primary"
-				onClick={() => handleSubmit(email, password)}
-				classes={{ root: styles['button-root'] }}
-			>
-				{intl.login.description}
-			</Button>
-		</div>
-	)
-
-	const renderHelpertext = () => (
-		<div className={styles['helper-text-container']}>
-			<FormHelperText error>{getLoginErrorMessage(errorCode)}</FormHelperText>
-		</div>
-	)
-
-	const renderWelcomeMessage = () => (
-		<div className={styles['welcome-message']}>{intl.welcome.description}</div>
 	)
 
 	return (
