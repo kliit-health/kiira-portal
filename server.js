200b1176a9ed1729fde04357a8e08f45fdaa6541
Steven Taglohner <steven.taglohner@gmail.com>
2020-07-01 14:09:45 -0700

    applies minor rebase fixes

--- server.js
+++ server.js
@@ -1,23 +1,16 @@
 const admin = require('firebase-admin')
 const functions = require('firebase-functions')
 const next = require('next')
-const config = require('./next.config')
+const conf = require('./next.config')
 
 admin.initializeApp()
 
 const dev = process.env.NODE_ENV !== 'production'
-const app = next({
-	dev,
-	// the absolute directory from the package.json file that initialises this module
-	// IE: the absolute path from the root of the Cloud Function
-	conf: config
-})
+const app = next({ dev, conf })
 const handle = app.getRequestHandler()
 
-const server = functions.https.onRequest((request, response) => {
-	// log the page.js file or resource being requested
-	console.log('File: ' + request.originalUrl)
-	return app.prepare().then(() => handle(request, response))
-})
+const server = functions.https.onRequest((request, response) =>
+	app.prepare().then(() => handle(request, response))
+)
 
 exports.nextjs = { server }
