0e83853df21f34a90ee0baac611c7a490b410ef7
Steven Taglohner <steven.taglohner@gmail.com>
2020-07-10 15:40:49 -0700

    adds helpers functions to clone child, parse csv and rename obj keys

---
+++ src/helpers/constants/index.js
@@ -0,0 +1,11 @@
+// pathnames
+
+export const DASHBOARD_PAGE = '/dashboard'
+export const LOGIN_PAGE = '/'
+export const HOME = '/'
+export const NOT_FOUND = '/404'
+
+export const CREATED_AT = 'createdAt'
+export const EMAIL = 'email'
+export const DISPLAY_NAME = 'displayName'
+export const SIGNED_UP_DATE = 'signedUpDate'
--- src/helpers/functions/parseCSV.js
+++ src/helpers/functions/parseCSV.js
@@ -1,20 +1,38 @@
 import Papa from 'papaparse'
 import { PARSE_CSV_ERRORS } from 'errors'
+import { renameObjectKeys } from './renameObjectKeys'
+const { MISSING_FILE, BAD_FILE } = PARSE_CSV_ERRORS
 
-const { MISSING_FILE } = PARSE_CSV_ERRORS
-
-export const parseCSV = file =>
+export const parseCsv = (file, keysMap) =>
 	new Promise((resolve, reject) => {
 		if (!file) reject(MISSING_FILE)
 		Papa.parse(file, {
 			header: true,
 			skipEmptyLines: true,
 			worker: true,
-			complete: result => {
-				resolve(result)
+			complete: ({ data, errors }) => {
+				if (errors.length > 0) {
+					reject(BAD_FILE)
+					return
+				}
+				const keys = Object.keys(keysMap)
+				const validated = data.map(item => {
+					keys.forEach(key => {
+						const missingProperty = !item.hasOwnProperty(key)
+						if (missingProperty) {
+							reject('bad file', key, item)
+							return
+						}
+					})
+					return renameObjectKeys(keysMap, item)
+				})
+				resolve(validated)
+				return
 			},
 			error: error => {
-				reject(error)
+				console.error(error)
+				reject(BAD_FILE)
+				return
 			}
 		})
 	})
