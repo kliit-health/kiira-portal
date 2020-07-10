9920882a29bf9c6eeb0a90ed622d04f063d0c37b
Steven Taglohner <steven.taglohner@gmail.com>
2020-07-10 15:48:13 -0700

    removes link component

--- src/components/link/Link.js
+++
@@ -1,9 +0,0 @@
-import NextLink from 'next/link'
-import classnames from 'classnames'
-import './styles.scss'
-
-export const Link = ({ classes = {}, href, name, as }) => (
-	<NextLink passHref as={as} href={href}>
-		<a className={classnames('link', classes.root)}>{name}</a>
-	</NextLink>
-)
