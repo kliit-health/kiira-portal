830f20b300203ec9e064e50bed9a7c30e43698c6
Aaron Elkins <akatoll@hotmail.com>
2020-07-17 10:14:52 -0700

    Add Forgot password to login screen

--- src/firebase/functions.js
+++ src/firebase/functions.js
@@ -21,6 +21,19 @@ export const signIn = (email, password) =>
 
 export const signOut = () => auth.signOut()
 
+export const forgotPassword = email =>
+	new Promise((resolve, reject) =>
+		auth
+			.setPersistence(PERSISTANCE.SESSION)
+			.then(() =>
+				auth
+					.sendPasswordResetEmail(email)
+					.then(response => resolve(response))
+					.catch(error => reject(error))
+			)
+			.catch(error => reject(error))
+	)
+
 export const getUserDetails = (uid, collectionName = 'users') =>
 	new Promise((resolve, reject) => {
 		const collection = firestore.collection(collectionName)
--- src/firebase/index.js
+++ src/firebase/index.js
@@ -3,6 +3,7 @@ export {
 	getUserDetails,
 	signIn,
 	signOut,
+	forgotPassword,
 	firebaseSimpleFetch,
 	createUsers
 } from './functions'
