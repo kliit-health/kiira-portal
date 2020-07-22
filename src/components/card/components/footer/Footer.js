acc0948e9a38ddf6ef3cbaed2e5f1d06cecae71f
Steven Taglohner <steven.taglohner@gmail.com>
2020-07-22 00:25:33 -0700

    updates card design

--- src/components/card/components/footer/Footer.js
+++ src/components/card/components/footer/Footer.js
@@ -1,10 +1,15 @@
 import { Divider } from 'src/components'
+import { FOOTER } from 'src/helpers/constants'
 import classnames from 'classnames'
 import './styles.scss'
 
-export const Footer = ({ children, classes = {}, divider }) => (
+const Footer = ({ children, classes = {}, divider }) => (
 	<div className={classnames('card-footer', classes.root)}>
 		{divider && <Divider />}
 		{children}
 	</div>
 )
+
+Footer.displayName = FOOTER
+
+export default Footer
