1a62ba93a9cc058372c261ec36f9d78c3b694a48
Steven Taglohner <steven.taglohner@gmail.com>
2020-08-02 18:54:27 -0700

    minor ui fixes

--- src/components/cancelButton/CancelButton.js
+++ src/components/cancelButton/CancelButton.js
@@ -6,6 +6,7 @@ export const CancelButton = ({ onCancel, classes = {} }) => {
 		button: classnames('cancel-button', classes.root),
 		image: classnames('cancel-button__image', classes.image)
 	}
+
 	return (
 		<div className={styles.button} onClick={onCancel}>
 			<img className={styles.image} src="/assets/cancel.svg" />
