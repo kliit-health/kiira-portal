f37242d638eec41c5d0e684cf9c41b97701e8615
Steven Taglohner <steven.taglohner@gmail.com>
2021-06-18 10:56:49 -0700

    updates engine

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -10,7 +10,7 @@ import storage from 'redux-persist/lib/storage'
 let store
 
 const persistConfig = {
-	key: 'kiira-portal-dev',
+	key: 'kiira-portal-prod',
 	whitelist: [],
 	storage // place to select which state you want to persist
 }
